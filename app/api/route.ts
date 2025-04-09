79cc3a52303a3984992a1c953e7d0a47f2081a83
mostafa <mostafa.h.web@gmail.com>
2025-04-09 14:30:39 +0300

    write logic of route handler for giving data seprately

--- app/api/route.ts
+++ app/api/route.ts
@@ -1,9 +1,54 @@
+import { NextRequest } from 'next/server';
 import {data} from '../data/Risloo-Center-Sample'
 
-export async function GET() {
+type Avatar = {
+    id: string;
+    file_name: string;
+    slug: string;
+    mode: string;
+    url: string;
+    type: string;
+    mime: string;
+    exec: string;
+  };
+  
+  interface Manager {
+    id: string;
+    name: string;
+    user_id: string;
+    avatar: Avatar[];
+  }
+  
+  interface Detail {
+    title: string;
+    description: string;
+    address: string;
+    avatar: Avatar[];
+    phone_numbers: string[];
+  }
+  
+  interface Item {
+    id: string;
+    manager: Manager;
+    acceptation: null;
+    status: string;
+    type: string;
+    detail: Detail;
+    created_at: number;
+  }
 
-    
-    return new Response(JSON.stringify(data), {
+export async function GET(req: NextRequest) {
+
+    const numPerReq: number = 10;
+    const currentPage: number = Number(req.nextUrl.searchParams.get('page'));
+
+    const start: number = ((currentPage * numPerReq) - numPerReq);
+    const end: number = (currentPage * numPerReq) -1;
+
+    const filteredData: {}[] = data.filter((_item: {}, index: number) => (index >= start && index <= end))
+    // console.log(filteredData)
+
+    return new Response(JSON.stringify(filteredData), {
         headers: {
             "content-type" : "application/json"
         }
