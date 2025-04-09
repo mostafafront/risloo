76c1d9361e86aea2103df1d2bf8e4cfc924bd89c
mostafa <mostafa.h.web@gmail.com>
2025-04-09 12:15:41 +0300

    create hero section and import it in home page

---
+++ app/components/organism/heroSection.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function HeroSection() {
+  return (
+    <>
+      <div className="py-5">
+        <h1 className="text-3xl font-bold text-center">مراکز روانشناسی</h1>
+      </div>
+    </>
+  );
+}
